e4733f5ba0bfc3f62fad0bd032321c5811fc263f
Another-Chris <91467693+Another-Chris@users.noreply.github.com>
2022-08-01 16:52:16 +0800

    start to train

--- util_scripts/project.py
+++ util_scripts/project.py
@@ -5,10 +5,11 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-import pandas as pd 
+import pandas as pd
 
 
-d = '../save/RawNet3_SSL/feature'
+# d = '../save/RawNet3_SSL/feature'
+d = '../save/ResNetSE34L_SSL/feature'
 with open(f'{d}/features.pkl', 'rb') as f:
     features = pickle.load(f)
 
@@ -16,13 +17,14 @@ with open(f'{d}/labels.pkl', 'rb') as f:
     labels = pickle.load(f)
 
 
-spks = [f'id0080{i}' for i in range(5)]
-for i in range(len(labels)):
+spks = [f'id0080{i}' for i in range(6)]
+
+for i in range(200, len(labels)):
     if labels[i] not in spks:
-        break 
-    
-labels = labels[:i]
-features = features[:i]
+        break
+
+labels = labels[200:i]
+features = features[200:i]
 
 features = np.stack(features, axis=0)
 
@@ -30,7 +32,6 @@ proj = TSNE(learning_rate='auto', init='pca').fit_transform(features)
 
 df = pd.DataFrame({'x1': proj[:, 0], 'x2': proj[:, 1], 'label': labels})
 
-plt.figure(figsize = (10,5))
-sns.scatterplot(data = df, x = 'x1', y = 'x2', hue = 'label')
+plt.figure(figsize=(10, 5))
+sns.scatterplot(data=df, x='x1', y='x2', hue='label')
 plt.show()
-

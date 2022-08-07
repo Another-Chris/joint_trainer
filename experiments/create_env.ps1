67b23760637f9f759a51423b2f6d95c867db4312
Another-Chris <91467693+Another-Chris@users.noreply.github.com>
2022-08-07 10:54:04 +0800

    remove draws folder

--- experiments/create_env.ps1
+++
@@ -1,5 +0,0 @@
-virtualenv venv;
-cd ./venv/Scripts;
-./activate.ps1;
-cd ../../;
-pip install -r requirements.txt
--- experiments/rawnet3/eval_cnceleb.ps1
+++
@@ -1,7 +0,0 @@
-python ./trainSpeakerNet.py `
---eval `
---config ./configs/RawNet3_SimSLR.yaml `
---save_path ./save/RawNet3_SSL `
---test_path data/cnceleb/eval `
---test_list data/test_list_cnceleb.txt `
---initial_model ./pre_trained/RawNet3.pt ` #pre-trained weights
